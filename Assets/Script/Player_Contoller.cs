b035816db08812a5a76f7c0e14ec5c1f188aa2cb
nxnxmi0-afk <nxnxmi0@gmail.com>
2026-04-21 10:21:52 +0700

    save

---
+++ Assets/Script/Player_Contoller.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float Speed; //public variable use Pascal casing
+    float move; //private variable
+    
+    public float JumpForce;
+    public bool IsJumping;
+    
+    Rigidbody2D rb2d;
+
+    // Unity Message | 0 references
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    // Unity Message | 0 references
+
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal"); //x - axis
+        //use rigidbody2d to move left and right (x-axis)
+        rb2d.linearVelocity = new Vector2(move * Speed, rb2d.linearVelocity.y);
+
+        //jump
+        if (Input.GetButtonDown("Jump") && !IsJumping)
+        {
+            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, JumpForce));
+            Debug.Log("Jump"); //for debugging purpose
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            IsJumping = false;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            IsJumping = true;
+        }
+    }
+}
+
